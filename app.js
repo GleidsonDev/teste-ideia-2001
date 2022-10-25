f76f1ace36fc6328541c283478f48362ab6cba34
GleidsonDEv <gleluis15@gmail.com>
2022-10-24 23:31:30 -0300

    Subindo arquivos

---
+++ app.js
@@ -0,0 +1,11 @@
+function acao() {
+  let modal = document.querySelector('.popup');
+
+  modal.style.display = 'block';
+}
+
+function fechar() {
+  let modal = document.querySelector('.popup');
+
+  modal.style.display = 'none';
+}
